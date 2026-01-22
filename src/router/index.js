361f189907bff27aa8e64dd08c71bda42ac409a9
belal <toumybelal@gmail.com>
2026-01-22 10:30:19 +0300

    update 10

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 // import { createRouter, createWebHistory } from "vue-router";
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter , createWebHashHistory } from "vue-router";
 
 import { isAuth } from "../Auth/authfunc";
 
